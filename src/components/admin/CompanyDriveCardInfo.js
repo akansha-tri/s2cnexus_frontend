3f812bde3b6d32acc13d68d0b76a5ce8fc5c14a0
Akansha Tripathi <akanshatripathi2601@gmail.com>
2023-07-22 16:10:14 +0530

    new

--- src/components/admin/CompanyDriveCardInfo.js
+++ src/components/admin/CompanyDriveCardInfo.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const AdminDriveInfo = () => {
     useEffect(() => {
-        axios.get('https://s2c-nexus-3gq5.onrender.com/company/driveInfo', {
+        axios.get('https://s2c-nexus-backend.onrender.com/company/driveInfo', {
           params: {
             driveId: localStorage.getItem("activeAdminDriveId")
           }
